e98b9e26b63459fe1f3499deabe3fc00919cf353
giovannaluizefonseca <giovannafonsecaluize@gmail.com>
2024-09-03 17:09:20 -0300

    Questão da fazenda no python

---
+++ exemplo4.py
@@ -0,0 +1,6 @@
+nomefazenda = input("Informe o nome da fazenda:")
+qtdcavalos = float(input("Informe a quantidade de cavalos:"))
+
+qtdferraduras = qtdcavalos * 4
+
+print(f"A fazenda {nomefazenda} possui {qtdcavalos} cavalos, a quantidade total de ferraduras para equipar todos os cavalos é {qtdferraduras}")
